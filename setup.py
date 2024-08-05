230317bb51095eb3fca9e01dcb75f3c7887c9e40
kimanthijona <kimanthijona@gmail.com>
2024-08-06 00:14:18 +0300

    setup

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
-from setuptools import find_packages,setup
-import typing from List
+from setuptools import find_packages, setup
+from typing import List
 
-HYPEN_E_DOT='-e.'
-def get_requirements(file_path:str)->List[str]:
-    ''''
-    This function returns the list of requirements
-    ''''
-    requirements=[]
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    Returns the list of Requirements
+    """
+    requirements = []
     with open(file_path) as file_obj:
-    requirements=file_obj.readlines()
-    requirements=[req.replace("\n","") for req in requirements]
-    
-    if HYPEN_E_DOT in requirements:
-        requirements.remove(HYPEN_E_DOT)
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
         
     return requirements
 
 setup(
-name='BISystem',
-version='0.1',
-author='Jona',
-author_mail='kimanthijona@gmail.com',
-packages=find_packages()
-install_requires=get_requirements('requirements.txt')
-
+    name='BISystem',
+    version='0.1',
+    author='Jonathan',
+    author_email='kimanthijona@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
-
